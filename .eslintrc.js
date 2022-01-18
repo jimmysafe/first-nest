49b5f48678e3635b7e9d6218a56badc49b2bf838
Jimmy <jimmy@Jimmys-Mac-mini.home>
2022-01-18 22:14:43 +0100

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,10 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'unused-imports',
   ],
   root: true,
   env: {
